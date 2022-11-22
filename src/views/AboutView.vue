d9438de48055a1d5eb8a0872c551878e755317ed
kizunas <8s6s7e@gmail.com>
2022-11-22 17:41:10 +0900

    toRefs

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,26 +2,41 @@
   <div class="about">
     <h1>{{name}}</h1>
     <h1>{{age}}</h1>
-    <h1>{{nameRef}}</h1>
+    <h1>ref:{{nameRef}}</h1>
+    <h1>reactive:{{book.title}}</h1>
+    <h1>reactive:{{book.author[0]}}</h1>
+    <h1>reactivToRefs:{{titleRef}}</h1>
+    <h1>reactivToRefs:{{authorRef[1]}}</h1>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive, toRefs } from 'vue'
 
 export default {
   data(){},
   setup(){
     let name = '大谷'
     const age = 30
+
     const nameRef = ref('錦織')
-    console.log(nameRef)
-    console.log(nameRef.value)
+
+    const book = reactive({
+      title: 'タイトル',
+      author: ['大谷', '伊藤']
+    })
+
+    const booktoRefs = reactive({
+      titleRef: 'タイトル2',
+      authorRef: ['大谷2', '伊藤2']
+    })
 
     return {
       name,
       age,
-      nameRef
+      nameRef,
+      book,
+      ...toRefs(booktoRefs)
     }
   }
 }
