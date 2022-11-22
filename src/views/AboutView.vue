2f8c4be732bb0666e29ff06c654d733edfbb7002
kizunas <8s6s7e@gmail.com>
2022-11-22 16:47:57 +0900

    setuup()の書き方

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{name}}</h1>
+    <h1>{{age}}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data(){},
+  setup(){
+    let name = '大谷'
+    const age = 30
+
+    return {
+      name: name,
+      age: age,
+    }
+  }
+}
+</script>
