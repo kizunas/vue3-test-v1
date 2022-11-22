e07b1435d23653353ff90430f13d7e0962f56da7
kizunas <8s6s7e@gmail.com>
2022-11-22 16:02:46 +0900

    teleport

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/childrenView">ChildrenView</router-link>
+    <router-link to="/childrenView">ChildrenView</router-link> |
+    <router-link to="/teleport-test">Tleport</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ src/views/TeleportTest.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    teleport
+    <ModalButton />
+  </div>
+</template>
+
+<script>
+import ModalButton from '@/components/ModalButton'
+
+export default {
+  components:{
+    ModalButton
+  } 
+}
+</script>
+
+<style>
+
+</style>
