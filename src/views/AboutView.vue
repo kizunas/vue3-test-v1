4a4ca9334ce161c1db0370514121607383ac8245
kizunas <8s6s7e@gmail.com>
2022-11-22 17:02:59 +0900

    refについて

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,19 +2,26 @@
   <div class="about">
     <h1>{{name}}</h1>
     <h1>{{age}}</h1>
+    <h1>{{nameRef}}</h1>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   data(){},
   setup(){
     let name = '大谷'
     const age = 30
+    const nameRef = ref('錦織')
+    console.log(nameRef)
+    console.log(nameRef.value)
 
     return {
-      name: name,
-      age: age,
+      name,
+      age,
+      nameRef
     }
   }
 }
