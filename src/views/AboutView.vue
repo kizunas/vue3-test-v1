3b3d906664da1cb520d807df0cfd8c08254a0a1e
kizunas <8s6s7e@gmail.com>
2022-11-23 16:43:41 +0900

    watchEffect

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,12 +10,13 @@
     <button @click="btnClick">クリック</button>
     <button @click="btnClick2">クリック2</button>
     <h1>computed:{{totalPrice}}</h1>
-    <div><input v-model="search"></div>
+    <div>watch:<input v-model="search"></div>
+    <div>watchEffect:<input v-model="searchEffect"></div>
   </div>
 </template>
 
 <script>
-import { ref, reactive, toRefs, computed, watch } from 'vue'
+import { ref, reactive, toRefs, computed, watch, watchEffect } from 'vue'
 
 export default {
   data(){},
@@ -23,18 +24,22 @@ export default {
     let name = '大谷'
     const age = 30
 
+  //ref
     const nameRef = ref('錦織')
 
+  //reactive
     const book = reactive({
       title: 'タイトル',
       author: ['大谷', '伊藤']
     })
 
+  //toRefs
     const booktoRefs = reactive({
       titleRef: 'タイトル2',
       authorRef: ['大谷2', '伊藤2']
     })
 
+  //method
     const btnClick = () => {
       console.log(book.title)
     }
@@ -43,6 +48,7 @@ export default {
       console.log(e)
     }
 
+  //computed
     const item = reactive({
       price: 100,
       number: 1
@@ -52,12 +58,21 @@ export default {
       return item.price * item.number
     })
 
+  //watch
     const search = ref('')
     watch (search, (newValue, preValue) => {
       console.log(`watch: ${search.value}`)
       console.log(`newValue: ${newValue}`)
       console.log(`preValue: ${preValue}`)
     })
+
+  //watchEffect
+    const searchEffect = ref('')
+    watchEffect(()=>{
+      console.log(`watchEffect: ${searchEffect.value}`)
+    })
+
+
 
     return {
       name,
@@ -69,7 +84,8 @@ export default {
       btnClick2,
       item,
       totalPrice,
-      search
+      search,
+      searchEffect
     }
   }
 }
